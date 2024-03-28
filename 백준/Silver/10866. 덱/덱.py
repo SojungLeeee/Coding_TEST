7b3efec79201bb8084f6f1ee3a61d7bbff3ae881
SojungLeeee <164296796+SojungLeeee@users.noreply.github.com>
2024-03-28 18:10:02 +0900

    Update 덱.py

--- 백준/Silver/10866. 덱/덱.py
+++ 백준/Silver/10866. 덱/덱.py
@@ -1,49 +1,96 @@
+#deque 사용하여 문제 풀기
+
+from collections import deque
 import sys
-n=int(sys.stdin.readline().strip())
-deck=[]
+
+d=deque()
+n=int(input())
 
 for i in range(n):
-    command=list(sys.stdin.readline().split())
+    command=sys.stdin.readline().split()
 
     if command[0]=='push_front':
-        deck.insert(0,(int(command[1])))
-        # print(deck) 잘들어가는지 확인용
-
+        d.appendleft(int(command[1]))
     elif command[0]=='push_back':
-        deck.append(int(command[1]))
-        # print(deck) 확인용
-
+        d.append(int(command[1]))
     elif command[0]=='pop_front':
-        if deck:
-            print(deck.pop(0))
+        if d:
+            print(d.popleft())
         else:
             print('-1')
-    
     elif command[0]=='pop_back':
-        if deck:
-            print(deck.pop())
+        if d:
+            print(d.pop())
         else:
             print('-1')
-
     elif command[0]=='size':
-        print(len(deck))
-    
+        print(len(d))
     elif command[0]=='empty':
-        if deck:
+        if d:
             print('0')
         else:
             print('1')
 
     elif command[0]=='front':
-        if deck:
-            print(deck[0])
+        if d:
+            print(d[0])
         else:
             print('-1')
 
     elif command[0]=='back':
-        if deck:
-            print(deck[-1])
+        if d:
+            print(d[-1])
         else:
-            print('-1')
+            print('-1') 
+
+
+
+# import sys
+# n=int(sys.stdin.readline().strip())
+# deck=[]
+
+# for i in range(n):
+#     command=list(sys.stdin.readline().split())
+
+#     if command[0]=='push_front':
+#         deck.insert(0,(int(command[1])))
+#         # print(deck) 잘들어가는지 확인용
+
+#     elif command[0]=='push_back':
+#         deck.append(int(command[1]))
+#         # print(deck) 확인용
+
+#     elif command[0]=='pop_front':
+#         if deck:
+#             print(deck.pop(0))
+#         else:
+#             print('-1')
+    
+#     elif command[0]=='pop_back':
+#         if deck:
+#             print(deck.pop())
+#         else:
+#             print('-1')
+
+#     elif command[0]=='size':
+#         print(len(deck))
+    
+#     elif command[0]=='empty':
+#         if deck:
+#             print('0')
+#         else:
+#             print('1')
+
+#     elif command[0]=='front':
+#         if deck:
+#             print(deck[0])
+#         else:
+#             print('-1')
+
+#     elif command[0]=='back':
+#         if deck:
+#             print(deck[-1])
+#         else:
+#             print('-1')
 
     
