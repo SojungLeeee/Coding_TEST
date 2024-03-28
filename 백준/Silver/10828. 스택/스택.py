3d45b9bfa912ad8eecfe94c81dec11d6a2d023fc
SojungLeeee <164296796+SojungLeeee@users.noreply.github.com>
2024-03-28 15:36:52 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,34 @@
+import sys
+n=int(sys.stdin.readline().strip())
+stack=[]
+
+for i in range(n):
+    command=list(sys.stdin.readline().split())
+
+    if command[0]=='push':
+        stack.append(int(command[1]))
+
+    elif command[0]=='pop':
+        if stack:
+            print(stack.pop(-1))
+        else:
+            print('-1')
+
+    elif command[0]=='size':
+        print(len(stack))
+
+    elif command[0]=='empty':
+        if stack:
+            print('0')
+        else:
+            print('1')
+
+    elif command[0]=='top':
+        if stack:
+            print(stack[-1])
+        else:
+            print('-1')
+
+
+
+        
