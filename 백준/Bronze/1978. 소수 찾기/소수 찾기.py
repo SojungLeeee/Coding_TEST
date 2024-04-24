6210ee5c13b418813bf5585f46457b67aa7a5804
SojungLeeee <164296796+SojungLeeee@users.noreply.github.com>
2024-04-24 14:26:43 +0900

    [Bronze II] Title: 소수 찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,11 +1,17 @@
-n=int(input())
-num_list=list(map(int,input().split()))
-sosu=0
-for num in num_list: #리스트 내 원소 목록 차례대로 불러오기
-    if num>1: #만약 원소가 1보다 크다면 (1은 소수x)
-        for i in range(2,num): #나누는 수를 2부터 num-1까지 돌도록
-            if num%i==0: #만약 원소가 2~num-1수로 나누어지면
+import sys
+
+n=int(sys.stdin.readline())
+n_list=list(map(int,sys.stdin.readline().split()))
+count=0
+
+for num in n_list:
+    if num==1:
+        count+=1
+    else:
+        for i in range(2,num):
+            if num%i==0:
+                count+=1
                 break
-        else:
-            sosu+=1
-print(sosu)
+
+print(len(n_list)-count)
+
