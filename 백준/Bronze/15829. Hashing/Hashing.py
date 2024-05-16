9b0eeeb2291c36078240f2bf4c70d3edb40e31ee
SojungLeeee <164296796+SojungLeeee@users.noreply.github.com>
2024-05-16 14:02:45 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,21 +1,25 @@
 import sys
-sum=0
-count=0
+n=int(sys.stdin.readline())
+sentence=(sys.stdin.readline().strip())
 
-l=int(sys.stdin.readline())
+
+r=31
+M=1234567891
+
 words=['a','b','c','d','e','f','g','h','i','j','k','l',
        'm','n','o','p','q','r','s','t','u','v','w','x','y','z']
 words_num=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
            21,22,23,24,25,26]
 
-sentence=sys.stdin.readline().strip()
+count=0
+sum=0
 
 for i in sentence:
     for j in words:
         if i==j:
             check_index=words.index(j)
             check_words_num=words_num[check_index]
-            sum+=check_words_num*(31**(count))
+            sum+=check_words_num*(r**(count))
             count+=1
 
-print(sum)
+print(sum%M)
