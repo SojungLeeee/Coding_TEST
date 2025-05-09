9528f624386e3d539d74614b4bde77758ad9bc16
Sojung Lee <164296796+SojungLeeee@users.noreply.github.com>
2025-05-09 17:56:53 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 3.22 ms, Memory: 89.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+        for(int i=0; i<s.length(); i++){
+            for(int j=i+1; j<s.length(); j++){
+                if(s.charAt(i)==s.charAt(j)){
+                    s=s.replace(String.valueOf(s.charAt(i))," ");
+                }
+            }            
+        }
+        
+        s=s.replace(" ","");
+        
+        String[] s_arr = s.split("");
+        Arrays.sort(s_arr);
+        
+        for(int i=0; i<s_arr.length; i++){
+            answer+=s_arr[i];
+        }
+        
+        return answer;
+    }
+}
