87ca53675d25b1222f2108c03cb76d860d849bb5
Sojung Lee <164296796+SojungLeeee@users.noreply.github.com>
2025-05-14 13:08:41 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.04 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] answer = {0,0};
+        int maxHeight = board[1]/2;
+        int maxWidth = board[0]/2;
+        
+        for(int i=0; i<keyinput.length; i++){
+            if(keyinput[i].equals("left")){
+                if(answer[0]-1>-maxWidth){
+                    answer[0] = answer[0]-1;
+                }
+                else{
+                    answer[0] = -maxWidth;
+                }
+            }
+            else if(keyinput[i].equals("right")){
+                if(answer[0]+1<maxWidth){
+                    answer[0] = answer[0]+1;
+                }
+                else{
+                    answer[0] = maxWidth;
+                }
+            }
+            else if(keyinput[i].equals("up")){
+                if(answer[1]+1<maxHeight){
+                    answer[1] = answer[1]+1;
+                }
+                else{
+                    answer[1] = maxHeight;
+                }
+            }
+            else if(keyinput[i].equals("down")){
+                if(answer[1]-1>-maxHeight){
+                    answer[1] = answer[1]-1;
+                }
+                else{
+                    answer[1] = -maxHeight;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
