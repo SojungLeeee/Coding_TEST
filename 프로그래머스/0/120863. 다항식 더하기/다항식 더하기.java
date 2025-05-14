8adfa8537adfbeb40a358e0c62c76fbd252a5992
Sojung Lee <164296796+SojungLeeee@users.noreply.github.com>
2025-05-14 13:55:07 +0900

    [level 0] Title: 다항식 더하기, Time: 10.40 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
@@ -1,7 +1,8 @@
 class Solution {
     public String solution(String polynomial) {
         String answer = "";
-        String[] arr = polynomial.split(" ");
+        polynomial = polynomial.replace("+","");
+        String[] arr = polynomial.split("  ");
         
         int xNum = 0;
         int num = 0;
@@ -12,9 +13,6 @@ class Solution {
             }
             else if(arr[i].contains("x")&&arr[i].length()!=1){
                 xNum += Integer.parseInt(String.valueOf(arr[i].replace("x","")));
-            }
-            else if(arr[i].equals("+")){
-                continue;
             }
             else {
                 num += Integer.parseInt(String.valueOf(arr[i]));
