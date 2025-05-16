cb5658e2d3d700ea017514db9f3af9f65ad77b35
Sojung Lee <164296796+SojungLeeee@users.noreply.github.com>
2025-05-16 21:32:29 +0900

    [level 0] Title: 이진수 더하기, Time: 0.03 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
@@ -3,34 +3,12 @@ import java.util.*;
 class Solution {
     public String solution(String bin1, String bin2) {
         String answer = "";
-        int num = 0;
-        int num2 = 0;
-        for(int i=0; i<bin1.length(); i++){
-            int count = bin1.length()-i;
-            num += Integer.parseInt(String.valueOf(bin1.charAt(i)))*Math.pow(2,count-1);
-        }
+        int bin1_10 = Integer.parseInt(bin1,2);
+        int bin2_10 = Integer.parseInt(bin2,2);
         
-        for(int i=0; i<bin2.length(); i++){
-            int count = bin2.length()-i;
-            num2 += Integer.parseInt(String.valueOf(bin2.charAt(i)))*Math.pow(2,count-1);
-        }
+        int sum = bin1_10 + bin2_10;
         
-        //10진수로 바꾼 후 10진수 sum 값 : num+num2
-        int sum = num + num2;
-        
-        if (sum == 0) return "0";
-        
-        List<Integer> list = new ArrayList<>();
-        
-        while(sum > 0){
-            list.add(sum%2);
-            sum /= 2;
-        }
-        
-        for(int i=list.size()-1; i>=0; i--){
-            answer += String.valueOf(list.get(i));
-        }
-        
+        answer = Integer.toBinaryString(sum);
         return answer;
     }
 }
