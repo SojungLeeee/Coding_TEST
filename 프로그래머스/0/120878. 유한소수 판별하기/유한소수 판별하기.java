5c97e7db20c389df214c440f4b1cf50472cd75a3
Sojung Lee <164296796+SojungLeeee@users.noreply.github.com>
2025-05-16 15:43:13 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.12 ms, Memory: 89.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 2;
+
+        // 유클리드 호제법으로 최대공약수 구하기
+        int x = a;
+        int y = b;
+        while (y != 0) {
+            int temp = y;
+            y = x % y;
+            x = temp;
+        }
+        // x가 최대공약수(GCD)
+        b /= x;  // 기약분수의 분모
+
+        // 분모가 2와 5로만 이루어져 있는지 확인
+        
+        while (b % 2 == 0) b /= 2;
+        while (b % 5 == 0) b /= 5;
+
+        if (b == 1) {
+            answer = 1;  // 유한소수
+        }
+
+        return answer;
+    }
+}
