387a69b64b727b99fcdb8a496a8ab1bcdecd7932
Sojung Lee <164296796+SojungLeeee@users.noreply.github.com>
2025-05-19 16:41:33 +0900

    [level 0] Title: 로그인 성공?, Time: 0.02 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.java
@@ -1,7 +1,5 @@
 class Solution {
-    public String solution(String[] id_pw, String[][] db) {
-        String answer = "fail";
-        
+    public String solution(String[] id_pw, String[][] db) {        
         for(int i=0; i<db.length; i++){
             if(id_pw[0].equals(db[i][0])){
                 if(id_pw[1].equals(db[i][1])){
@@ -12,7 +10,6 @@ class Solution {
                 }
             }
         }
-        
-        return answer;
+        return "fail";
     }
 }
