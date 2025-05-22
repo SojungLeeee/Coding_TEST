00b2e8eb4fd6c5bcb13c9831dd1d62893b8427d5
Sojung Lee <164296796+SojungLeeee@users.noreply.github.com>
2025-05-23 01:20:18 +0900

    [level 2] Title: 의상, Time: 0.10 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,33 +2,23 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String, List<String>> map = new HashMap<>();
-
-        for (int i = 0; i < clothes.length; i++) {
-            String item = clothes[i][0];      // 의상 이름
-            String category = clothes[i][1];  // 의상 종류
-
-            // 카테고리가 이미 존재하면 리스트에 추가하고
-            if (map.containsKey(category)) {
-                map.get(category).add(item);
-                //map.get(category) : 리스트 형태로 반환
-                //list.add(item);
-            } 
-            
-            // 존재하지 않으면 새 리스트를 만들어서 put
-            else {
-                List<String> list = new ArrayList<>();
-                list.add(item);
-                map.put(category, list);
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<clothes.length; i++){
+            String type = clothes[i][1];
+            if(map.containsKey(type)){
+                map.put(type, map.get(type)+1);
+            }
+            else{
+                map.put(type,1);
             }
         }
         
         int answer = 1;
-        for(List<String> list : map.values()){
-            answer *= list.size() + 1;
+        for(Integer num : map.values()){
+            answer *= num+1;
         }
         
-        return answer-1; //아무것도 안입는 경우를 제외
-        
+        return answer-1;
     }
 }
