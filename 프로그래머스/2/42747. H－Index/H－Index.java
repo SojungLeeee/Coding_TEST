401211f8f4eaa12466acd20537c104a1296d966f
Sojung Lee <164296796+SojungLeeee@users.noreply.github.com>
2025-06-03 00:38:06 +0900

    [level 2] Title: H-Index, Time: 0.71 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        
+        //논문의 개수
+        int n = citations.length;
+        
+        //[0,1,3,5,6]
+        Arrays.sort(citations);
+        
+        for(int i=0; i<n; i++){
+            //i번째부터 끝까지 논문의 개수
+            int overCitations = n-i;
+            if(citations[i]>=overCitations){
+                answer = overCitations;
+                break;
+            }
+        }
+    
+        return answer;
+    }
+}
