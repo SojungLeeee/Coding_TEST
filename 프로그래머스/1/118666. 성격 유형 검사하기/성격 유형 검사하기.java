11b5f369cc3b35ac14c81680e023e9aeae80b641
Sojung Lee <164296796+SojungLeeee@users.noreply.github.com>
2025-06-16 17:00:35 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 4.10 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -27,50 +27,32 @@ class Solution {
             }
             else if(choices[i]<4){
                 String s = survey[i].substring(0,1);
-                int addScore = 0;
-                if(choices[i]==1){
-                    addScore = 3;
-                }
-                else if(choices[i]==2){
-                    addScore = 2;
-                }
-                else if(choices[i]==3){
-                    addScore = 1;
-                }
-                
+                int addScore = 4-choices[i];                
                 score.put(s, score.get(s)+addScore);
             }
         }
         
         //각각 비교
-        if(score.get("R")<score.get("T")){
-            answer += "T";
-        }
-        else if(score.get("R")>=score.get("T")){
-            answer += "R";
-        }
-        
-        if(score.get("C")<score.get("F")){
-            answer += "F";
-        }
-        else if(score.get("C")>=score.get("F")){
+        if (score.get("R") >= score.get("T"))
+            answer = "R";
+        else
+            answer = "T";
+
+        if (score.get("C") >= score.get("F"))
             answer += "C";
-        }
-        
-        
-        if(score.get("J")<score.get("M")){
-            answer += "M";
-        }
-        else if(score.get("J")>=score.get("M")){
+        else
+            answer += "F";
+
+        if (score.get("J") >= score.get("M"))
             answer += "J";
-        }
-        
-        if(score.get("A")<score.get("N")){
-            answer += "N";
-        }
-        else if(score.get("A")>=score.get("N")){
+        else
+            answer += "M";
+
+        if (score.get("A") >= score.get("N"))
             answer += "A";
-        }
+        else
+            answer += "N";
+
         
         return answer;
     }
